9c3d0035aff75b364e25d44ac2c96e6935a1d9c8
Stanislav Kunt <stanislav.kunt@gmail.com>
2024-11-04 12:38:32 +0100

    manifest handling

---
+++ common/models/manifest.ts
@@ -0,0 +1,14 @@
+export interface Manifest {
+  Author: string
+  Title: string
+  AppId: string
+  Description: string
+  Version: string
+  Icon: string
+  MinSize: number
+  Weight: number
+  BaseUrl: string
+  IndexPage: string
+  Cache: string
+  ContainerServices: any[]
+}
--- frontend/handful.ts
+++ frontend/handful.ts
@@ -1,4 +1,48 @@
+import { Guid } from "helpers/stringextender";
+import { Manifest } from "../common/models/manifest";
+
+const APPTOKEN = "apptoken";
+
+/**
+ * frontend class to simplyfy access to Straca's services
+ */
 export class StracaInHandful
 {
+    constructor()
+    {
+    }
+
     
+    /**
+     * gets app token.
+     * app token is id of the application installation on the device. 
+     */
+    get appToken()
+    {
+        var token = localStorage.getItem(APPTOKEN);
+        if(token == null)
+        {
+            token = Guid.newGuid();
+            localStorage.setItem(APPTOKEN,token);
+        }
+        return token;
+    }
+
+    protected _manifest:Manifest;
+    /**
+     * get's app's manifest
+     * @returns manifest
+     */
+    async getManifest():Promise<Manifest>
+    {
+        if(this._manifest == null)
+        {
+            const response = await fetch("/manifest.json");
+            this._manifest = (await response.json()) as Manifest;
+        }
+        return this._manifest;
+    }
+
+
+
 }
